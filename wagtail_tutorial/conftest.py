49979c6f527ee35c33800a04deeeaa9cffde0037
thevvalverde <fvalverde007@gmail.com>
2022-09-07 09:04:01 +0100

    Wagtail tutorial git repository. Starting from 'Your first wagtail site' tutorial, using cookiecutter and docker

---
+++ wagtail_tutorial/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from wagtail_tutorial.users.models import User
+from wagtail_tutorial.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
